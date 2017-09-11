72f85e7812bb3fb9f10e7d9370686b8887a76bc5
kazuya_katayama <kazuya_katayama@cybird-8.local>
2017-09-11 11:40:31 +0900

    9/11

--- fuel/app/views/home/index.php
+++ fuel/app/views/home/index.php
@@ -1,7 +1,2 @@
 スマホ以外から見ています。
-
-<?php echo $count ?>回目のロードです
-<script>
-alert(<?php echo $count ?>);
-</script>
 <?php var_dump($_SERVER['HTTP_USER_AGENT']); ?>
--- fuel/app/views/home/mb_index.php
+++ fuel/app/views/home/mb_index.php
@@ -1,7 +1,2 @@
 スマホから見ています。
-
-<?php echo $count ?>回目のロードです
-<script>
-alert(<?php echo $count ?>);
-</script>
 <?php var_dump($_SERVER['HTTP_USER_AGENT']); ?>
