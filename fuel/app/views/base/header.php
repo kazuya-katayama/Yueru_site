0af8fc5978bc30bd3dc425542260efd89cbd3569
kazuya_katayama <kazuya_katayama@cybird-8.local>
2017-09-12 15:24:39 +0900

    menu tab

--- fuel/app/views/base/header.php
+++ fuel/app/views/base/header.php
@@ -2,3 +2,16 @@
 <img src='assets/img/sirayuki.jpg' class="head-img1"/>
 <img src='assets/img/ai_yuzuki.jpg' class="head-img2"/>
 </div>
+<ul class="menu-">
+  <li class="menu-case">
+    <div class="menu-name">Menu 1</div>
+    <ul class="menu-list">
+      <li class="menu-one">Menu 1-1</li>
+      <li class="menu-one">Menu 1-2</li>
+      <li class="menu-one">Menu 1-3</li>
+    </ul>
+  </li>
+  <li class="menu-case">-*-*- 略 -*-*-</li>
+  <li class="menu-case">-*-*- 略 -*-*-</li>
+  <li class="menu-case">-*-*- 略 -*-*-</li>
+</ul>
