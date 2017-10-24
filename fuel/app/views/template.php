356411315166e208e1af7ce0499378bdce8194c4
kazuya_katayama <kazuya_katayama@DigitalQuest-no-MacBook-Pro1.local>
2017-10-24 20:13:17 +0900

    html基本タグ修正

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -1,33 +1,29 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <?php //metas.phpファイルを読み込む ?>
     <!--metas-->
-    <?php echo $metas; ?>
+    <?php echo $metas;  //metas.phpファイルを読み込む?>
     <!--//metas-->
   </head>
   <body>
+    <!--container-->
     <div id="container">
-      <?php //header.phpファイルを読み込む ?>
       <!--header-->
-      <?php echo $header; ?>
+        <?php echo $header; //header.phpファイルを読み込む?>
+        <?php echo $header_menu; //header_menu.phpファイルを読み込む?>
       <!--//header-->
-        <div id="wrapper">
-            <div id="contents">
-              <?php //メインコンテンツの読み込み ?>
-              <?php echo $content; ?>
-            </div>
-        <?php //side_menu.phpを読み込み ?>
-        <?php if (isset($side_menu)) {?>
-          <!--side_menu-->
-          <?php echo $side_menu; ?>
-          <!--//side_menu-->
-        <?php } ?>
-        <?php //side_menu.phpを読み込み ?>
-        <!--footer-->
-        <?php echo $footer; ?>
-        <!--//footer-->
+      <div id="contents">
+        <?php echo $content;  //メインコンテンツの読み込み?>
       </div>
+      <?php if (isset($side_menu)) {?>
+        <!--side_menu-->
+        <?php echo $side_menu;  //side_menu.phpを読み込み?>
+        <!--//side_menu-->
+      <?php } ?>
+      <!--footer-->
+        <?php echo $footer; //footer.phpを読み込み?>
+      <!--//footer-->
     </div>
+    <!--//container-->
   </body>
 </html>
