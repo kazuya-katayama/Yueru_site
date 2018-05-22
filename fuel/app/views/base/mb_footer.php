9bee3a266bbdcfa18d561f140981bfefff73269e
root <root@tk2-251-35012.vs.sakura.ne.jp>
2018-05-22 19:37:15 +0900

    cssの修正

--- fuel/app/views/base/mb_footer.php
+++ fuel/app/views/base/mb_footer.php
@@ -1,3 +1,27 @@
+<p id="pagetop"><a href="#site_top"><img src="images/img_pagetop.png" height="70" width="70" alt="ページトップボタン"></a></p>
+<script>
+	$(function(){
+    	// 「ページトップへ戻るボタン」の要素を隠します
+    	$('#pagetop').hide();
+    	// スクロールした場合のアクションが記されています
+    	$(window).scroll(function(){
+        	// スクロール位置が200pxを超えた場合に「ページトップへ戻るボタン」をフェードインで出現させる
+        	if ($(this).scrollTop() &gt; 200) {
+            	$('#pagetop').fadeIn();
+        	}
+        	// スクロール位置が200px以下の場合は「ページトップへ戻るボタン」を消しておく（フェードアウトで消える）
+        	else {
+            	$('#pagetop').fadeOut();
+        	}
+    	});
+    	// 「ページトップへ戻るボタン」をクリックした場合のページトップへ戻るスピードの速さが記されています
+    	$('#pagetop').click(function(){
+        	$('html,body').animate({
+            	scrollTop: 0 }, 500);
+        	return false;
+    	});
+	});
+</script>
 <div id="footer">
   <div class="copyright">
     <div class="floatL pl5 pt10">Copyright©Yueru.Company co.,ltd. All Rights Reserved.</div>
--- fuel/app/views/base/mb_header.php
+++ fuel/app/views/base/mb_header.php
@@ -1,4 +1,4 @@
-<div class="header-top-group">
+<div class="header-top-group" id="site_top">
   <img src='/assets/img/sirayuki.jpg' class="head-img1"/>
   <div class="head-name">ゆえるのサイト ここの画像募集中及び作成中</div>
 </div>
